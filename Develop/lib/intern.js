e135b3c55d0ecb7d5d45e2c2d229279985222026
cprom <chen.prom@gmail.com>
2020-02-20 19:31:02 -0800

    update logic

---
+++ Develop/lib/intern.js
@@ -0,0 +1,14 @@
+class Intern extends Employee {
+  constructor(name, id, title, school) {
+    super(name, id, title)
+    this.school = school
+  }
+  getSchool() {
+
+  }
+  getRole() {
+
+  }
+}
+
+module.exports = Intern
